5272c9017cd86f19a4a03f2621389ba52509f4f9
nikita242114 <5358711@mail.ru>
2024-03-10 16:31:59 +0500

    work1

---
+++ JavaCoreHomeWork/src/main/java/logics/Logic.java
@@ -0,0 +1,7 @@
+package logics;
+
+public class Logic {
+    public static double indexCount(double weight, double height) {
+        return weight/Math.pow(height, 2);
+    }
+}
---
+++ JavaCoreHomeWork/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import decorators.Decorator;
+import logics.Logic;
+import views.Conole;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Double> data = Conole.userData();
+        double result = Logic.indexCount(data.get(0), data.get(1));
+        Decorator.decorate(result);
+    }
+}
